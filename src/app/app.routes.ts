bc12e3e2e85ab27d838bf8096726961219b7efa0
JorgeLo1 <jeyle222@gmail.com>
2024-05-16 12:47:38 -0500

    Front

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import {LayoutComponent} from "@shared/components/layout/layout.component";
 import {AboutComponent} from "./domains/info/pages/about/about.component";
 import {NotFoundComponent} from "./domains/info/pages/not-found/not-found.component";
 import {LoginComponent} from "@products/pages/login/login.component";
+import { PasarelaComponent } from '@products/pages/pasarela/pasarela.component';
 
 export const routes: Routes = [
   {
@@ -24,6 +25,10 @@ export const routes: Routes = [
     {
     path:'login',
     component: LoginComponent
+  },
+  {
+    path:'pasarela',
+    component: PasarelaComponent
   },
   {
     path: '**',
