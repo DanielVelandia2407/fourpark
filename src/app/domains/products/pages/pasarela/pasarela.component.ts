ee229b923cbfb55293102e35abf61bc50fb468c3
JorgeLo1 <jeyle222@gmail.com>
2024-05-19 22:36:46 -0500

    Mostrar datos consola

--- src/app/domains/products/pages/pasarela/pasarela.component.ts
+++ src/app/domains/products/pages/pasarela/pasarela.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
 
+import { FormGroup, FormControl, Validators, ReactiveFormsModule  } from '@angular/forms';
+
 @Component({
   selector: 'app-pasarela',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './pasarela.component.html',
   styleUrl: './pasarela.component.css'
 })
 export class PasarelaComponent {
+
+  pasarelaForm = new FormGroup({
+    nombreTitular: new FormControl('', Validators.required),
+    numeroTarjeta : new FormControl('', Validators.required),
+    fechaMes : new FormControl('', Validators.required),
+    fechaAnio : new FormControl('', Validators.required),
+    cvc : new FormControl('', Validators.required)
+  })
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+
+  onPasarela(form:any): void{
+    console.log(form)
+  }
 
 }
