253f905de823a695623df37c4c5ea9620b9d106f
JorgeLo1 <jeyle222@gmail.com>
2024-05-20 20:51:31 -0500

    consola 2.0

--- src/app/domains/products/pages/pasarela/pasarela.component.ts
+++ src/app/domains/products/pages/pasarela/pasarela.component.ts
@@ -13,18 +13,16 @@ export class PasarelaComponent {
 
   pasarelaForm = new FormGroup({
     nombreTitular: new FormControl('', Validators.required),
-    numeroTarjeta : new FormControl('', Validators.required),
-    fechaMes : new FormControl('', Validators.required),
-    fechaAnio : new FormControl('', Validators.required),
+    number : new FormControl('', Validators.required),
+    expiration_date : new FormControl('', Validators.required),
     cvc : new FormControl('', Validators.required)
   })
 
   constructor() { }
 
   ngOnInit(): void {
-
   }
-
+  
   onPasarela(form:any): void{
     console.log(form)
   }
