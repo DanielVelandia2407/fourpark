ca6da6d0f864add5e59d6372c89e50aa3f0f0a96
DanielVelandia2407 <134900983+DanielVelandia2407@users.noreply.github.com>
2024-05-21 23:28:08 -0500

    Fix admin

--- src/app/domains/products/pages/login/login.component.ts
+++ src/app/domains/products/pages/login/login.component.ts
@@ -3,6 +3,7 @@ import {LoginService} from "@shared/login/login.service";
 import {Router} from '@angular/router';
 import {CommonModule} from "@angular/common";
 import {FormControl, FormGroup, ReactiveFormsModule} from "@angular/forms";
+import Swal from 'sweetalert2';
 
 
 @Component({
@@ -14,27 +15,31 @@ import {FormControl, FormGroup, ReactiveFormsModule} from "@angular/forms";
 })
 export class LoginComponent {
 
-    formulario: FormGroup;
+  formulario: FormGroup;
 
-    loginService = inject(LoginService);
+  loginService = inject(LoginService);
 
-    constructor(private router: Router){
-      this.formulario = new FormGroup({
-        user_name: new FormControl,
-        password: new FormControl,
+  constructor(private router: Router) {
+    this.formulario = new FormGroup({
+      user_name: new FormControl,
+      password: new FormControl,
+    });
+  }
+
+  async onSubmit() {
+    try {
+      const response = await this.loginService.postLogin(this.formulario.value);
+      console.log(response);
+      if (response && response.token) {
+        localStorage.setItem('token', response.token)
+      }
+      this.router.navigateByUrl('');
+    } catch (error) {
+      Swal.fire({
+        title: "Error!",
+        text: "La combinación de usuario y contraseña no es correcta",
+        icon: "warning"
       });
     }
-
-    async onSubmit(){
-      try {
-        const response = await this.loginService.postLogin(this.formulario.value);
-        console.log(response);
-        if (response && response.token) {
-          localStorage.setItem('token', response.token)
-        }
-        this.router.navigateByUrl('');
-      } catch (error) {
-        console.error(error);
-      }
-    }
+  }
 }
