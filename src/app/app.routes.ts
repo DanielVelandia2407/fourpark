6519ef1c247b9c5f53f3c0ee6ce3c78453beb0b2
JorgeLo1 <jeyle222@gmail.com>
2024-05-21 22:41:35 -0500

    proceso c y f

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,12 @@
 import {Routes} from '@angular/router';
-
 import {ListComponent} from "./domains/products/pages/list/list.component";
 import {LayoutComponent} from "@shared/components/layout/layout.component";
 import {AboutComponent} from "./domains/info/pages/about/about.component";
 import {NotFoundComponent} from "./domains/info/pages/not-found/not-found.component";
 import {LoginComponent} from "@products/pages/login/login.component";
 import { PasarelaComponent } from '@products/pages/pasarela/pasarela.component';
+import {CompletadoComponent} from '@products/pages/pasarela/proceso/completado/completado.component'
+import { RechazadoComponent } from '@products/pages/pasarela/proceso/rechazado/rechazado.component';
 
 export const routes: Routes = [
   {
@@ -29,6 +30,14 @@ export const routes: Routes = [
   {
     path:'pasarela',
     component: PasarelaComponent
+  },
+  {
+    path:'procesoc',
+    component: CompletadoComponent
+  },
+  {
+    path:'procesor',
+    component: RechazadoComponent
   },
   {
     path: '**',
--- src/app/domains/products/pages/pasarela/pasarela.component.ts
+++ src/app/domains/products/pages/pasarela/pasarela.component.ts
@@ -1,53 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Card } from '../../../../services/admin/data.service';
-import { FormGroup, FormControl, Validators, ReactiveFormsModule  } from '@angular/forms';
-import { DataService } from '../../../../services/admin/data.service'
-//import { pasarelaI } from '@shared/models/pasarela.interface';
+import { FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
+import { DataService } from '../../../../services/admin/data.service';
+import { Router } from '@angular/router'; 
 
 @Component({
   selector: 'app-pasarela',
   standalone: true,
   imports: [ReactiveFormsModule],
   templateUrl: './pasarela.component.html',
-  styleUrl: './pasarela.component.css'
+  styleUrls: ['./pasarela.component.css'] 
 })
-
-export class PasarelaComponent {
+export class PasarelaComponent implements OnInit {
 
   pasarelaForm = new FormGroup({
     nombreTitular: new FormControl('', Validators.required),
-    number : new FormControl('', Validators.required),
-    expiration_date : new FormControl('', Validators.required),
-    cvc : new FormControl('', Validators.required)
-  })
+    number: new FormControl('', Validators.required),
+    expiration_date: new FormControl('', Validators.required),
+    cvc: new FormControl('', Validators.required)
+  });
 
-  constructor(private api:DataService) {}
+  card: Card;
 
-  card:  Card  
+  constructor(private api: DataService, private router: Router) {} 
 
-  ngOnInit(form:any): void {
+  ngOnInit(): void {
     this.api.getOptionsCards().subscribe((card: Card) => {
       this.card = card;
       console.log(this.card);
-
     });
   }
 
-  
-  
-  onPasarela(form:any): void{
-
-    if(form.cvc === this.card.cvc &&
-      form.number === this.card.number &&
-      form.expiration_date === this.card.expiration_date
-    ){
+  onPasarela(form: any): void {
+    if (form.cvc === this.card.cvc &&
+        form.number === this.card.number &&
+        form.expiration_date === this.card.expiration_date) {
       console.log("ok");
-    }else{
-      console.log("No")
+      this.router.navigate(['/procesoc']); 
+    } else {
+      console.log("No");
+      this.router.navigate(['/procesor']); 
     }
-
   }
-    
-  
-
 }
